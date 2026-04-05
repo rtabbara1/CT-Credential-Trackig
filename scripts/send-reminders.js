31f4a0a8a8ae829c47aaefd7658081fcd40fa3de
atrius-imaging <270201724+atrius-imaging@users.noreply.github.com>
2026-04-05 18:47:45 -0400

    Fetch techs from data.json instead of hardcoded array

--- scripts/send-reminders.js
+++ scripts/send-reminders.js
@@ -1,26 +1,20 @@
 import nodemailer from "nodemailer";
+import https from "https";
 const GMAIL_USER = process.env.OUTLOOK_USER;
 const GMAIL_PASSWORD = process.env.OUTLOOK_PASSWORD;
 const SUPERVISOR_EMAIL = "Raghda_Tabbara@atriushealth.org";
 const MANAGER_EMAIL = "Cheryl_Whyte@atriushealth.org";
-const TECHS = [
-  { firstName: "Karen", lastName: "Calabro", email: "Karen_Calabro@atriushealth.org", arrt: "2026-07-31", stateLicense: "2027-08-31", bls: "2027-03-01" },
-  { firstName: "Christina", lastName: "Desmond", email: "Christina_Desmond@atriushealth.org", arrt: "2026-07-31", stateLicense: "2027-08-31", bls: "2026-10-01" },
-  { firstName: "Adam", lastName: "Dykas", email: "Adam_Dykas@atriushealth.org", arrt: "2027-07-31", stateLicense: "2027-08-31", bls: "2027-05-01" },
-  { firstName: "Anthony", lastName: "Gattonini", email: "Anthony_Gattonini@atriushealth.org", arrt: "2026-08-31", stateLicense: "2027-09-30", bls: "2026-10-01" },
-  { firstName: "Ibrahim", lastName: "Haboub", email: "Ibrahim_Haboub@atriushealth.org", arrt: "2027-08-31", stateLicense: "2027-09-30", bls: "2027-06-01" },
-  { firstName: "Stacey", lastName: "Hurley", email: "Stacey_Hurley@atriushealth.org", arrt: "2027-05-31", stateLicense: "2027-06-30", bls: "2027-05-01" },
-  { firstName: "Stacie", lastName: "Joyce", email: "Stacie_Joyce@atriushealth.org", arrt: "2026-05-31", stateLicense: "2027-06-30", bls: "2028-02-01" },
-  { firstName: "Mary", lastName: "Mello", email: "Mary_Mello@atriushealth.org", arrt: "2026-07-31", stateLicense: "2027-08-31", bls: "2027-03-01" },
-  { firstName: "Dona", lastName: "Menutole", email: "Dona_Menutole@atriushealth.org", arrt: "2027-01-31", stateLicense: "2027-02-28", bls: "2028-03-01" },
-  { firstName: "Kelley", lastName: "Secord", email: "Kelley_Secord@atriushealth.org", arrt: "2027-10-31", stateLicense: "2027-11-30", bls: "2026-10-01" },
-  { firstName: "Christine", lastName: "Sorrento", email: "Christine_Sorrento@atriushealth.org", arrt: "2026-10-31", stateLicense: "2027-11-30", bls: "2027-10-01" },
-  { firstName: "Nancy", lastName: "Spaulding", email: "Nancy_Spaulding@atriushealth.org", arrt: "2026-09-30", stateLicense: "2027-10-31", bls: "2027-03-01" },
-  { firstName: "Kevin", lastName: "Suslavicius", email: "Kevin_Suslavicius@atriushealth.org", arrt: "2027-07-31", stateLicense: "2027-08-31", bls: "2027-10-01" },
-  { firstName: "Raghda", lastName: "Tabbara", email: "Raghda_Tabbara@atriushealth.org", arrt: "2027-01-31", stateLicense: "2027-02-28", bls: "2026-09-30" },
-  { firstName: "Mary", lastName: "Thompson", email: "Mary_Thompson@atriushealth.org", arrt: "2026-10-31", stateLicense: "2027-11-30", bls: "2027-09-30" },
-  { firstName: "Cheryl", lastName: "Whyte", email: "Cheryl_Whyte@atriushealth.org", arrt: "2027-04-30", stateLicense: "2027-05-31", bls: "2026-12-01" },
-];
+function fetchJSON(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, { headers: { "User-Agent": "CT-Credentials/1.0" } }, res => {
+      let data = "";
+      res.on("data", c => data += c);
+      res.on("end", () => { try { resolve(JSON.parse(data)); } catch(e) { reject(e); } });
+    }).on("error", reject);
+  });
+}
+const { techs: TECHS } = await fetchJSON("https://raw.githubusercontent.com/atrius-imaging/CT-Credential-Tracking/main/data.json?t=" + Date.now());
+console.log("Loaded", TECHS.length, "techs from data.json");
 const CREDENTIALS = [{key:"arrt",label:"ARRT Certification"},{key:"stateLicense",label:"State License"},{key:"bls",label:"BLS Certification"}];
 const REMINDER_DAYS = [30,14,7];
 function daysUntil(d){const today=new Date();today.setHours(0,0,0,0);const exp=new Date(d+"T00:00:00");exp.setHours(0,0,0,0);return Math.ceil((exp-today)/86400000);}
